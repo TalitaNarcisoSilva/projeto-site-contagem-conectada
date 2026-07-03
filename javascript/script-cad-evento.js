7b071cc75c026aad17c128db3279549ab21bcdfe
Web-192 <cauasantiagofreitas23241@gmail.com>
2026-07-02 22:02:16 -0300

    javascript quase completo 2

--- javascript/script-cad-evento.js
+++ javascript/script-cad-evento.js
@@ -1,11 +1,51 @@
 const formulario = document.getElementById("newsletter");
 
-formulario.addEventListener("submit", function(event){
+formulario.addEventListener("submit", function (event) {
     //impede que a página recarregue
     event.preventDefault();
 
     //pega o valor do input
-    const email = document.getElementById("email").ariaValueMax;
+    const titulo = document.getElementById("nome");
+    const data = document.getElementById("data");
+    const descricao = document.getElementById("descricao");
+    const rua = document.getElementById("rua")
+    const numero = document.getElementById("numero")
+    const cidade = document.getElementById("cidade")
+    const cep = document.getElementById("cep")
+    const latitude = document.getElementById("latitude")
+    const longitude = document.getElementById("longitude")
 
-    alert("E-mail cadastrado; " + email);
-});
+    if (titulo.value.trim() == "") {
+        titulo.classList.add("is-invalid");
+        titulo.classList.remove("is-valid");
+    } else {
+        titulo.classList.add("is-valid");
+        titulo.classList.remove("is-invalid");
+    }
+
+    if (data.value.trim() == "") {
+        data.classList.add("is-invalid");
+        data.classList.remove("is-valid");
+    } else {
+        data.classList.add("is-valid");
+        data.classList.remove("is-invalid");
+    }
+
+    if (descricao.value.trim() == "") {
+        descricao.classList.add("is-invalid");
+        descricao.classList.remove("is-valid")
+    } else {
+        descricao.classList.add("is-valid");
+        descricao.classList.remove("is-invalid");
+    }
+
+    if (rua.value.trim() == "") {
+        rua.classList.add("is-invalid");
+        rua.classList.remove("is-valid");
+    } else {
+        data.classList.add("is-valid");
+        data.classList.remove("is-invalid");
+    }
+)
+
+}
